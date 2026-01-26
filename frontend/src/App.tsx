a40c3f0e23d0453a921a5b5730b69eab76635892
2192375d <173829746yzpq@gmail.com>
2026-01-26 16:27:18 -0500

    feat: styled basic front end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import PostCard from './component/post-card'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div>
       < PostCard title="this is a title!" content="some content" />
       {/* This is a page */}
-      <button onClick={() => setCount(count + 1)}> count = {count} </button >
+      {/* <button onClick={() => setCount(count + 1)}> count = {count} </button > */}
     </div >
   )
 }
