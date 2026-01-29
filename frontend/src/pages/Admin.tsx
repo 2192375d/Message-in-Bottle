cfa965a4157ec9d2c212f6eff28308e770756a3e
2192375d <173829746yzpq@gmail.com>
2026-01-29 13:51:04 -0500

    feat: admin redirect done

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Admin() {
-  return (<></>)
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function checkAuth() {
+      const res = await fetch("/api/admin/", { credentials: "include" });
+      const data = await res.json();
+      if (!data.is_admin) navigate("/adminlogin");
+    }
+    checkAuth();
+  }, [navigate]);
+
+  return <h1>ADMIN</h1>;
 }
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Ocean from "../pages/Ocean"
 import AdminLogin from "../pages/AdminLogin";
+import Admin from "../pages/Admin";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Ocean />}></Route>
       <Route path="/adminlogin" element={<AdminLogin />}></Route>
+      <Route path="/admin" element={<Admin />}></Route>
 
     </Routes>
   )
